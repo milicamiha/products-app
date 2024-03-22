52a2c2eb0bf90249c2b4f7660415fa3415ab463a
Mihajlovic Milica <mihajlovicmilica@yahoo.com>
2024-03-22 23:12:04 +0200

    Logger documentation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,17 @@
 const User = require('../models/user.model')
+const logger = require('../logger/logger')
 
 exports.findAll = async(req, res) => {
     console.log("Find all users")
 
     try{
     const result = await User.find()
-    res.status(200).json({data: result})
+    res.status(200).json({data: result});
+    logger.debug("Success in reading all users");
+    logger.info("Success in reading all users")
     } catch (err) {
         console.log(`Problem in reading users ${err}`)
+        logger.error(`Problem in reading all users, ${err}`)
     }
 }
 
